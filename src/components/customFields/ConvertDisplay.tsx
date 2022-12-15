a87df2530f4ae87c05519978bbdc2e6071889b12
Sam Kuhlmann <samkuhlmann@gmail.com>
2022-12-15 10:16:08 -0700

    Delete ConvertDisplay.tsx

--- src/components/customFields/ConvertDisplay.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-import { Buildable, Field } from "@daohaus/ui";
-
-export const ConvertDisplay = (props: Buildable<Field>) => {
-  console.log("pprops", props);
-  return <p>poopin</p>;
-};
