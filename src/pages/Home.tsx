11a9ac302b7a206b473d8907fa8fcaba679b939f
earth2travis <travis.mccutcheon@gmail.com>
2022-12-15 19:01:37 -0600

    Update home page placehodler content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,48 +1,89 @@
-import { H1, H2, Link, ParLg, SingleColumnLayout, Card } from '@daohaus/ui';
+import {
+  H1,
+  H2,
+  Button,
+  ParLg,
+  ParMd,
+  SingleColumnLayout,
+  Card,
+} from '@daohaus/ui';
 import styled from 'styled-components';
 
-const LinkBox = styled.div`
-  display: flex;
-  width: 50%;
-  justify-content: space-between;
-`;
+import { ButtonRouterLink } from '../components/ButtonRouterLink';
 
 const Cards = styled.div`
+  padding: 5em 0 5em 0;
   display: flex;
-  align-items: center;
   gap: 1.7rem;
 `;
 
 export const Home = () => {
   return (
     <SingleColumnLayout>
-      <H1>🐸 Frog Warning System</H1>
+      <H1>Lorem Ipsum</H1>
       <ParLg>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod
         praesentium iste nobis molestias maxime velit laudantium, facilis
         nesciunt minus illum sed necessitatibus laboriosam atque distinctio,
         assumenda at earum eius voluptate!
       </ParLg>
+      <Button onClick={function noRefCheck() {}}>Join DAO</Button>
       <Cards>
         <Card>
+          <H2>Proposals</H2>
+          <ParMd>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </ParMd>
+          <ButtonRouterLink
+            to={`/proposals`}
+            color='secondary'
+            linkType='no-icon-external'
+          >
+            View Proposals
+          </ButtonRouterLink>
+        </Card>
+        <Card>
+          <H2>Members</H2>
+          <ParMd>
+            Ex, iusto qui vitae asperiores ad ratione officiis! Magnam
+            praesentium veritatis nemo ab. Vitae deserunt eligendi molestias ea
+            reiciendis accusamus inventore facilis.
+          </ParMd>
+          <ButtonRouterLink
+            to={`/members`}
+            color='secondary'
+            linkType='no-icon-external'
+          >
+            See Members
+          </ButtonRouterLink>
+        </Card>
+        <Card>
           <H2>Treasury</H2>
-        </Card>
-        <Card>
-          <H2>Operations</H2>
-        </Card>
-        <Card>
-          <H2>Trading</H2>
+          <ParMd>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </ParMd>
+          <ButtonRouterLink
+            to={`/treasury`}
+            color='secondary'
+            linkType='no-icon-external'
+          >
+            View Treasury
+          </ButtonRouterLink>
         </Card>
       </Cards>
-      <LinkBox>
-        <Link href='https://github.com/HausDAO/monorepo' linkType='external'>
-          Github
-        </Link>
-        <Link href='https://admin.daohaus.fun/' linkType='external'>
-          Admin
-        </Link>
-        <Link href='/formtest'>Example Form</Link>
-      </LinkBox>
+      <H2>Lorem Ipsum</H2>
+      <ParLg>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sit
+        amet odio varius, tincidunt quam at, bibendum erat. Quisque tristique
+        sed sem sed iaculis. Etiam neque tellus, hendrerit ac ullamcorper vitae,
+        vestibulum quis dolor. In scelerisque nisi id pretium dictum. Ut sed
+        justo nulla. Duis faucibus arcu nec justo congue consequat. Morbi eu
+        lacus sed elit luctus tempor eu vel felis. Vestibulum efficitur ligula
+        non nulla feugiat, vitae ornare diam laoreet. Integer cursus erat nec
+        tincidunt lacinia. Phasellus quis leo lorem. Donec eleifend lorem a enim
+        tincidunt, at mollis risus congue. Proin feugiat consectetur fermentum.
+        Nam laoreet dolor nec pellentesque ultricies.
+      </ParLg>
     </SingleColumnLayout>
   );
 };
